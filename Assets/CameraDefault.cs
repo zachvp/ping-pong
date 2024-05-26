0dfb8220f4145bc228e0f4fcfb658343e84d2265
zachvp <vegaperk@gmail.com>
2024-05-26 12:14:33 -0600

    tighten up arena, movement

--- Assets/CameraDefault.cs
+++ Assets/CameraDefault.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 public class CameraDefault : MonoBehaviour
 {
     public GameObject target;
+    public Vector3 offsetFromTarget;
 
-    void Update()
+    void LateUpdate()
     {
-        transform.LookAt(target.transform);
+        var newPosition = transform.position;
+        newPosition.x = target.transform.position.x;
+        newPosition.y = target.transform.position.y;
+        newPosition += offsetFromTarget;
+
+        transform.position = newPosition;
+        //transform.LookAt(target.transform);
     }
 }
