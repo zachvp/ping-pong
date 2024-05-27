178923e569700d1537093db22590c13dab1988c8
zachvp <vegaperk@gmail.com>
2024-05-26 21:10:23 -0600

    update to latest unity editor; install unity multiplayer play mode

---
+++ Assets/Scripts/NetworkManagerGUI.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using System.Transactions;
+
+public class NetworkManagerGUI : MonoBehaviour
+{
+    private NetworkManager manager;
+
+    private void Awake()
+    {
+        manager = GetComponent<NetworkManager>();
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+        if (!manager.IsClient && !manager.IsServer)
+        {
+            if (GUILayout.Button("Start Host"))
+            {
+                manager.StartHost();
+            }
+            if (GUILayout.Button("Start Client"))
+            {
+                manager.StartClient();
+            }
+            if (GUILayout.Button("Start Server"))
+            {
+                manager.StartServer();
+            }
+        }
+        else
+        {
+            // status labels
+            string mode = "NONE";
+            if (manager.IsHost)
+            {
+                mode = "Host";
+            }
+            else if (manager.IsServer)
+            {
+                mode = "Server";
+            }
+            else if (manager.IsClient)
+            {
+                mode = "Client";
+            }
+
+            GUILayout.Label($"Transport: {manager.NetworkConfig.NetworkTransport.GetType().Name}");
+            GUILayout.Label($"Mode: {mode}");
+
+            // submit new position
+            if (GUILayout.Button(manager.IsServer ? "Move" : "Request Move"))
+            {
+
+
+            }
+        }
+
+        GUILayout.EndArea();
+    }
+}
