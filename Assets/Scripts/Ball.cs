87e9807d5a14f79a01f9a7e607984fba41ca2614
zachvp <vegaperk@gmail.com>
2024-05-28 00:32:05 -0600

    add tennis ball skin

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,91 +1,112 @@
 using UnityEngine.InputSystem;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class Ball : MonoBehaviour
 {
     public Vector3 initialVelocity;
 
     public float spinMultiplier = 1;
+    public float dashSpinMultiplier = 1.1f;
+    public float highVelocitySpinMultiplier = 1.5f;
     public Vector2 spinDampening = Vector2.one;
     public float hitMultiplier = 2;
+    public float torqueMultiplier = 5;
 
-    private Vector3 initialPosition;
 
     public Rigidbody Body { get { return body; } }
     private Rigidbody body;
 
-    private void Awake() {
+    private void Awake()
+    {
         body = GetComponent<Rigidbody>();
-        initialPosition = transform.position;
     }
 
-    private void Start() {
+    private void Start()
+    {
         body.velocity = initialVelocity;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         var newVelocity = body.velocity;
-        var playerCharacter = collision.gameObject.GetComponent<PlayerInputMapper>();
+        var contact = collision.contacts[collision.contacts.Length - 1];
+        var contactNormalDotForward = Vector3.Dot(contact.normal, Vector3.forward);
+        var contactNormalDotUp = Vector3.Dot(contact.normal, Vector3.up);
+        var contactNormalDotRight = Vector3.Dot(contact.normal, Vector3.right);
 
-        Debug.Log($"collision contacts: {collision.contacts.Length}");
-
-        foreach (var c in collision.contacts)
+        // check for a hit at either end
+        if (Mathf.Abs(contactNormalDotForward) > 0.9f)
         {
-            var contactNormalDotBack = Vector3.Dot(c.normal, Vector3.back); // body.velocity.normalized
-            var contactNormalDotUp = Vector3.Dot(c.normal, Vector3.up);
-            var contactNormalDotRight = Vector3.Dot(c.normal, Vector3.right);
-
-            if (Mathf.Abs(contactNormalDotBack) > 0.9f)
+            if (newVelocity.z > 0)
             {
-                if (newVelocity.z > 0)
+                newVelocity.z = Mathf.Max(initialVelocity.z, newVelocity.z);
+            }
+            else
+            {
+                newVelocity.z = Mathf.Min(-initialVelocity.z, newVelocity.z);
+            }
+
+            // ball collides with player
+            var playerCharacter = collision.gameObject.GetComponent<PlayerInputMapper>();
+            if (playerCharacter)
+            {
+                var playerVelocity = playerCharacter.FrameVelocity;
+                //Debug.Log($"player velocity: {playerVelocity}");
+
+                if (playerCharacter.state == PlayerInputMapper.State.HIT)
                 {
-                    newVelocity.z = Mathf.Max(initialVelocity.z, newVelocity.z);
+                    //Debug.Log($"apply hit boost");
+                    newVelocity.z *= hitMultiplier;
+                    newVelocity.z = Mathf.Clamp(newVelocity.z, -hitMultiplier * initialVelocity.z, hitMultiplier * initialVelocity.z);
+
+                    var spin = new Vector3(
+                        playerVelocity.x * spinMultiplier * hitMultiplier * spinDampening.x,
+                        playerVelocity.y * spinMultiplier * hitMultiplier * spinDampening.y,
+                        0);
+                    newVelocity -= spin;
+                }
+                else if (playerCharacter.state == PlayerInputMapper.State.DASH)
+                {
+                    newVelocity.x -= playerVelocity.x * spinMultiplier;
+                    newVelocity.y -= playerVelocity.y * spinMultiplier;
                 }
                 else
                 {
-                    newVelocity.z = Mathf.Min(-initialVelocity.z, newVelocity.z);
+                    newVelocity.x -= playerVelocity.x * spinMultiplier * spinDampening.x;
+                    newVelocity.y -= playerVelocity.y * spinMultiplier * spinDampening.y;
                 }
 
-                if (playerCharacter)
+                if (body.velocity.z > initialVelocity.z)
                 {
-                    var playerVelocity = playerCharacter.FrameVelocity;
-                    //Debug.Log($"player velocity: {playerVelocity}");
-
-                    if (playerCharacter.state == PlayerInputMapper.State.HIT)
-                    {
-                        //Debug.Log($"apply hit boost");
-                        newVelocity.z *= hitMultiplier;
-                        newVelocity.z = Mathf.Clamp(newVelocity.z, -hitMultiplier * initialVelocity.z, hitMultiplier * initialVelocity.z);
-
-                        var spinX = playerVelocity.x * spinMultiplier * hitMultiplier * spinDampening.x;
-                        var spinY = playerVelocity.y * spinMultiplier * hitMultiplier * spinDampening.y;
-                        //Debug.Log($"spinX: {spinX}, spinY: {spinY}");
-
-                        newVelocity.x -= spinX;
-                        newVelocity.y -= spinY;
-                    }
-                    else
-                    {
-                        newVelocity.x -= playerVelocity.x * spinMultiplier * spinDampening.x;
-                        newVelocity.y -= playerVelocity.y * spinMultiplier * spinDampening.y;
-                    }
-                }    
+                    newVelocity.x -= playerVelocity.x * highVelocitySpinMultiplier;
+                    newVelocity.y -= playerVelocity.y * highVelocitySpinMultiplier;
+                }
+                
+                if (playerVelocity.sqrMagnitude > Mathf.Epsilon)
+                {
+                    var torque = new Vector3(playerVelocity.y, -playerVelocity.x, 0) * torqueMultiplier;
+                    StartCoroutine(Task.FixedUpdate(() => body.AddTorque(torque)));
+                }
             }
+        }
 
-            if (Mathf.Abs(contactNormalDotUp) > 0.6f)
-            {
-                Debug.LogFormat($"contactNormalDotUp: {contactNormalDotUp}");
-                newVelocity.y *= 0.6f;
-                newVelocity.z = initialVelocity.z * Utils.SignMultiplier(newVelocity.z);
-            }
+        // check for steep upward/downward deflection
+        var steepDeflectValue = 0.65f;
+        var velocityNerf = 0.6f;
+        if (Mathf.Abs(contactNormalDotUp) > steepDeflectValue)
+        {
+            Debug.LogFormat($"contactNormalDotUp: {Mathf.Abs(contactNormalDotUp)}");
+            newVelocity.y *= velocityNerf;
+            newVelocity.z = initialVelocity.z * Utils.SignMultiplier(newVelocity.z);
+        }
 
-            if (Mathf.Abs(contactNormalDotRight) > 0.6f)
-            {
-                Debug.LogFormat($"contactNormalDotUp: {contactNormalDotUp}");
-                newVelocity.x *= 0.6f;
-                newVelocity.z = initialVelocity.z * Utils.SignMultiplier(newVelocity.z);
-            }
+        // check for steep leftward/rightward deflection
+        if (Mathf.Abs(contactNormalDotRight) > steepDeflectValue)
+        {
+            Debug.LogFormat($"contactNormalDotRight: {Mathf.Abs(contactNormalDotRight)}");
+            newVelocity.x *= velocityNerf;
+            newVelocity.z = initialVelocity.z * Utils.SignMultiplier(newVelocity.z);
         }
 
         body.velocity = newVelocity;
