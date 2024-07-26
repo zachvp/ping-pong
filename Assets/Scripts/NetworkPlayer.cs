d7692dfb81458d0e99d5b5349d842a2410d6ffd9
zachvp <vegaperk@gmail.com>
2024-07-25 18:41:59 -0600

    poc to replicate all client movement to all other clients

--- Assets/Scripts/NetworkPlayer.cs
+++ Assets/Scripts/NetworkPlayer.cs
@@ -1,22 +1,79 @@
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.InputSystem;
+using UnityEditor;
 
 public class NetworkPlayer : NetworkBehaviour
 {
-    public NetworkVariable<Vector3> velocity = new();
+    public NetworkVariable<Vector3> position = new();
+
     public Vector3 startPosition = new Vector3(0, 5, -1);
+    public float speed;
 
     public override void OnNetworkSpawn()
     {
-        UIDebug.Instance.Register($"OwnerClientID {OwnerClientId} OnNetworkSpawn", $"NetworkObjectId: {NetworkObjectId} IsOwner: {IsOwner} IsHost: {IsHost} IsClient: {IsClient}");
-        transform.position = startPosition;
+        UIDebug.Instance.Register($"ClientID {OwnerClientId} Spawned", $"ObjectId: {NetworkObjectId}, Host: {IsHost}, Owner: {IsOwner}, Client: {IsClient}");
+        
+        if (IsServer)
+        {
+        }
         //Debug.Log($"{nameof(NetworkPlayer)} OnNetworkSpawn: OwnerClientID: {OwnerClientId} NetworkObjectId: {NetworkObjectId} Is");
     }
 
-    //[ServerRpc]
-    //public void UpdateServerRPC(Vector3 newVelocity)
-    //{
-    //    Debug.LogFormat($"server received network update for object: {NetworkObjectId}; ownerClient: {OwnerClientId}");
-    //    velocity.Value = newVelocity;
-    //}
+    private void Start()
+    {
+        if (IsClient && IsOwner)
+        {
+            UIDebug.Instance.Register($"Client {OwnerClientId} network position", () => position.Value);
+            UIDebug.Instance.Register($"Client {OwnerClientId} client position", () => transform.position);
+
+            transform.position = startPosition;
+            UpdateServerRPC(startPosition);
+        }
+    }
+
+    private void Update()
+    {
+        if (IsServer)
+        {
+            transform.position += position.Value * Time.deltaTime;
+        }
+
+        if (IsClient && IsOwner)
+        {
+            var newPosition = PositionFromInput(Keyboard.current);
+            UpdateServerRPC(newPosition);
+        }
+    }
+
+    [ServerRpc]
+    public void UpdateServerRPC(Vector3 newPosition)
+    {
+        position.Value = newPosition;
+    }
+
+    private Vector3 PositionFromInput(Keyboard keyboard)
+    {
+        var direction = Vector3.zero;
+
+        if (keyboard.dKey.isPressed)
+        {
+            direction += Vector3.right;
+        }
+        if (keyboard.aKey.isPressed)
+        {
+            direction += Vector3.left;
+        }
+
+        if (keyboard.wKey.isPressed)
+        {
+            direction += Vector3.up;
+        }
+        if (keyboard.sKey.isPressed)
+        {
+            direction += Vector3.down;
+        }
+
+        return (speed * direction);
+    }
 }
